ba293870e3eda8ff4bff6816063ceeaa91c9e164
Abhijit Gaikwad <abhijitgaikwad1457@gmail.com>
2025-01-09 14:18:32 +0530

    First push

---
+++ Main.java
@@ -0,0 +1,23 @@
+// public class Main {
+//     public static void main(String[] args) {
+//       System.out.println("Hello World");
+//     }
+//   }
+
+
+
+public class Main{
+  public static void main(String[] argu){
+    String name1="abhijit";
+    String name2="gaikwad";
+    String name3="1457@gamil.com";
+    String name4=name1+name2+name3;
+    System.out.println(name4);
+
+    System.out.println(name1.charAt(0));
+
+    System.out.println(name1.replace('i',','));
+
+    System.out.println(name1.length(0));
+  }
+}
