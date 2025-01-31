323a19b67a5523d3c6bb468d25335525a7a97909
Abhijit Gaikwad <abhijitgaikwad1457@gmail.com>
2025-01-31 23:47:55 +0530

    inheritance

--- Dog.java
+++ Dog.java
@@ -6,11 +6,19 @@ class Animal{
     }
 }
 
-public class Inheritance extends Animal{
+class Birds extends Animal{
+    void birdSound(){
+        System.out.println("From Bird Class");
+    }
+}
+
+public class Dog extends Birds{
     public static void main(String[] args){
-        Inheritance obj1=new Inheritance();
+        Dog obj1=new Dog();
         System.out.println("From dog classssss");
         System.out.println(obj1.a+obj1.b);
         obj1.sound();
+        obj1.birdSound();
     }
 }
+
